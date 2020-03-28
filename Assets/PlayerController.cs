b5e0af8114d8f4610ad949aa9b433ad7d08fdda4
Olafur <olafurao16@ru.is>
2020-03-28 13:30:19 +0000

    first commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+
+    public List<GameObject> enemies;
+    public GameObject[] gameObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemies = new List<GameObject>();
+        gameObjects = GameObject.FindGameObjectsWithTag("Enemy");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if(Input.anyKeyDown)
+        {
+            foreach (var enemy in gameObjects)
+            {
+                Debug.LogWarning("BAM");
+
+                var cubeControllerScript = enemy.GetComponent<CubeController>();
+                Debug.LogWarning(cubeControllerScript.code);
+
+                if (Input.inputString == cubeControllerScript.code)
+                {
+                    cubeControllerScript.scanned = true;
+                }
+            }
+        }
+        */
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime); ;
+    }
+}
---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 1f;
+
+    public int moveHorizontalCounter = 0;
+    public int moveVerticalCounter = 0;
+
+    public string code;
+    public bool scanned = false;
+
+    public bool moveRight = true;
+    public bool moveLeft = false;
+    public bool moveUp = false;
+    public bool moveDown = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            //Debug.LogWarning(code);
+            //Debug.LogWarning(Input.inputString);
+            //Debug.LogWarning("///////////////////");
+
+            if (Input.inputString.Trim() == code)
+            {
+                scanned = true;
+                Destroy(gameObject);
+            }
+        }
+
+        int hor = 0, vert = 0;
+
+        if(moveHorizontalCounter < 120)
+        {
+            if (moveRight)
+            {
+                hor = 1;
+            }
+
+            else if(moveLeft)
+            {
+                hor = -1;
+            }
+        }
+
+        else
+        {
+            moveHorizontalCounter = 0;
+
+            if(moveRight)
+            {
+                moveRight = false;
+                moveLeft = true;
+            }
+            
+            else if(moveLeft)
+            {
+                moveLeft = false;
+                moveRight = true;
+            }
+            
+        }
+
+        if(!scanned)
+        {
+            Vector3 move = transform.forward * hor;
+            controller.Move(move * speed * Time.deltaTime);
+        }
+
+        moveHorizontalCounter += 1; 
+
+    }
+}
