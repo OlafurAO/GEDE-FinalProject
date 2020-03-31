309ea863e0107f1072b058cb4cabc57c62a17273
stebbi <stefant17@ru.is>
2020-03-31 12:42:48 +0000

    added random enemie location spawning

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,8 +9,7 @@ public class PlayerController : MonoBehaviour
 
     public List<GameObject> enemies = new List<GameObject>();
     public GameObject[] gameObjects;
-
-    Vector3 position0 = new Vector3(10, 4, 0);
+    
     Vector3 position1 = new Vector3(10, 4, 0);
     Vector3 position2 = new Vector3(15, 4, 0);
     Vector3 position3 = new Vector3(20, 4, 0);
@@ -24,16 +23,14 @@ public class PlayerController : MonoBehaviour
     public List<GameObject> wave2 = new List<GameObject>();
     public List<GameObject> wave3 = new List<GameObject>();
     public List<Vector3> locations = new List<Vector3>();
+    public List<Vector3> enemie_locations = new List<Vector3>();
 
     // Start is called before the first frame update
     void Start()
     {
-        fill_enemies(wave1);
-        fill_enemies(wave2);
-        fill_enemies(wave3);
         locations.Add(new Vector3(7, 4, -27));
-        locations.Add(new Vector3(21, 2, -6));
-        locations.Add(new Vector3(36, 4, 11));
+        locations.Add(new Vector3(21, 4, -6));
+        locations.Add(new Vector3(40, 4, 9));
         change_currWave(); 
     }
 
@@ -44,10 +41,25 @@ public class PlayerController : MonoBehaviour
         //gameObjects = GameObject.FindGameObjectsWithTag("Enemy");
         //gameObjects = currVave Enemies
         //strax og gameObjects eru 0. þá fer í næsta location og næsta wave 
-        if (clear() && isMoving == false)
+        if(controller.transform.position == locations[curr_position])
+        {
+            isMoving = false; 
+            if (curr_wave.Count == 0)
+            {
+                fill_enemies(curr_wave);
+            }
+
+        }else
+        {
+            isMoving = true;
+            move_player(); 
+        }
+
+
+        if (clear() == true && isMoving == false)
         {
             // nr hvaða possition playerinn á að vera í
-            if(curr_position <= 3)
+            if(curr_position < 3)
             {
                 curr_position ++;
             }
@@ -55,18 +67,10 @@ public class PlayerController : MonoBehaviour
             {
                 curr_position = 0;
             }
-            // fill gameObjects with new wave enemies 
-            change_currWave();
-        }
-        if(controller.transform.position != locations[curr_position])
-        {
             isMoving = true;
-           move_player();
-
-        }
-        else
-        {
-            isMoving = false; 
+            // fill gameObjects with new wave enemies 
+            //change_currWave();
+            change_currWave();
         }
 
     }
@@ -90,10 +94,12 @@ public class PlayerController : MonoBehaviour
         float z_pos = controller.transform.position.z;
         for(int nr_enemies= 0; nr_enemies < 5; nr_enemies++)
         {
+            int temp_int = 10 * nr_enemies;
+            Vector3 location = make_enemie_location();
             int rand = Random.Range(0, ens);
             GameObject temp_enemy = enemies[rand];
-            temp_enemy.SetActive(false); 
-            GameObject new_enemy = Instantiate(temp_enemy,new  Vector3(x_pos + 10, y_pos, z_pos+20), Quaternion.identity);
+            GameObject new_enemy = Instantiate(temp_enemy,location, Quaternion.identity);
+            new_enemy.SetActive(true); 
             //Destroy(enemies[rand]);
             wave.Add(new_enemy);
         }
@@ -111,10 +117,10 @@ public class PlayerController : MonoBehaviour
         {
             curr_wave = wave3;
         }
-        foreach (GameObject box in curr_wave)
-        {
-            box.SetActive(true);
-        }
+        //foreach (GameObject box in curr_wave)
+        //{
+        //    box.SetActive(true);
+        //}
     }
     
     bool clear()
@@ -127,5 +133,37 @@ public class PlayerController : MonoBehaviour
             }
         }
         return true;
+    }
+
+
+    Vector3 make_enemie_location()
+    {
+        float x_pos = controller.transform.position.x;
+        float y_pos = controller.transform.position.y;
+        float z_pos = controller.transform.position.z;
+        int random = Random.Range(0, 5);
+        if (random == 0)
+        {
+            return new Vector3(x_pos+ 10, y_pos, z_pos+ 10);
+        }else if(random == 1)
+        {
+            return new Vector3(x_pos+10, y_pos+5, z_pos+10);
+        }
+        else if (random == 2)
+        {
+            return new Vector3(x_pos-10, y_pos+10, z_pos+10);
+        }
+        else if (random == 3)
+        {
+            return new Vector3(x_pos-10, y_pos+5, z_pos+10);
+        }
+        else if (random == 4)
+        {
+            return new Vector3(x_pos-10, y_pos, z_pos-10);
+        }
+        else
+        {
+            return new Vector3(x_pos+5, y_pos+5, z_pos+5);
+        }
     }
 }
